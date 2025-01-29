0595a8b3d334215dcc9e2df76f7256c093cf5f4a
zzzhizhi <77013105+zzzhizhia@users.noreply.github.com>
2025-01-29 16:01:59 +0800

    style: add GitHub link

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -116,8 +116,10 @@ export async function Footer() {
             </div>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
-                &copy; {new Date().getFullYear()} zzzhizhi.
-                <PeekabooLink href=""> </PeekabooLink>
+                &copy; {new Date().getFullYear()} zzzhizhi. 网站已开源：
+                <PeekabooLink href="https://github.com/zzzhizhia/zzzhizhi.top">
+                  GitHub
+                </PeekabooLink>
               </p>
               <Links />
             </div>
