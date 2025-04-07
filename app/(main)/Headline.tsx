444f5e53d0c81ff14b0a55ab17e22a3f0bcdf4ac
zzzhizhi <77013105+zzzhizhia@users.noreply.github.com>
2025-04-07 20:22:20 +0800

    fix: email

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -121,7 +121,7 @@ export function Headline() {
         />
         <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
         <SocialLink
-          href="mailto:mail@zzzhizhi.top"
+          href="mailto:i@zzzhizhi.top"
           aria-label="我的邮箱"
           platform="mail"
         />
