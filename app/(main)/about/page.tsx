03743340f5668104e5f27d33d08fc5e5eb14b47d
zzzhizhi <77013105+zzzhizhia@users.noreply.github.com>
2025-05-04 19:52:30 +0800

    添加曾用域名 zzzhizhi.top

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -149,6 +149,7 @@ export default function AboutPage() {
 
         <h2>域名</h2>
         <p><strong>zzzhizhi.com</strong>：个人域名</p>
+        <p><strong>zzzhizhi.top</strong>：曾用域名</p>
         <p><strong>drim.cc</strong>：社团域名</p>
         <p><strong>lpslps.com</strong>：MC 服务器域名</p>
 
