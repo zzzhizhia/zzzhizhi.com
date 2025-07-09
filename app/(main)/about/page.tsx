1656488f3adf8eea4bddb5d1f36b358a1e5a2fe3
zzzhizhi <77013105+zzzhizhia@users.noreply.github.com>
2025-07-09 21:57:44 +0800

    docs(about): add AJAZZ AK820 MAX ULTRA to equipment list

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -155,6 +155,7 @@ export default function AboutPage() {
 
         <h2>装备</h2>
         <ul>
+          <li>AJAZZ AK820 MAX ULTRA</li>
           <li>iPhone 12 Pro</li>
           <li><s>Vivo X9s</s></li>
           <li>MECHREVO Jiguang Pro (2023)</li>
